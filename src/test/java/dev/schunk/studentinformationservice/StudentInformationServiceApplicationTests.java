e6654a238fe218abd25ccc4efdbc2050ecc435f0
Nikolai Schunk <nikolai@schunk.dev>
2023-07-04 11:27:53 +0200

    Springboot Initialization

---
+++ src/test/java/dev/schunk/studentinformationservice/StudentInformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.schunk.studentinformationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
